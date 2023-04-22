d9ecc263d819d25aca7641fec8827eea63717ba8
Kevin Chen <kevchen21@gmail.com>
2023-04-21 19:26:34 -0700

    initial commit

---
+++ ocr/models/bounding-box.ts
@@ -0,0 +1,10 @@
+import { IBoundingBox } from "../interfaces/bounding-box-interface";
+
+export class BoundingBox implements IBoundingBox {
+    constructor(
+        readonly top: number,
+        readonly left: number,
+        readonly width: number,
+        readonly height: number
+    ) { }
+}
